54336ca0671cc3839e3c626e1ea607af26a7f290
Reugin <abhigaikwad75@gmail.com>
2023-06-21 02:20:42 +0530

    no any chages done

--- scripts/post.js
+++ scripts/post.js
@@ -9,15 +9,17 @@ function enableEditMode() {
     if (postTitle.contentEditable === 'false') {
       postTitle.contentEditable = 'true';
       postText.contentEditable = 'true';
+      postText.disabled = false;
       postTitle.style.border = '1px solid pink';
       postText.style.border = '1px solid pink';
-      editButton.innerHTML = '<i class="fas fa-save"></i>Save';
+      editButton.innerHTML = 'Save <i class="fas fa-save"></i>';
     } else {
       postTitle.contentEditable = 'false';
       postText.contentEditable = 'false';
+      postText.disabled =true;
       postTitle.style.border = 'none';
       postText.style.border = 'none';
-      editButton.innerHTML = '<i class="fas fa-edit"></i>Edit';
+      editButton.innerHTML = 'Edit <i class="fas fa-edit"></i>';
     }
   }
   
@@ -50,13 +52,14 @@ function enableEditMode() {
   function addComment() {
     const commentInput = document.querySelector('.comment-input');
     const allComments = document.querySelector('.all-comments');
-  
+    allComments.style.display = "none";
     if (commentInput.value !== '') {
       const commentText = commentInput.value;
+     
       const commentElement = document.createElement('div');
       commentElement.classList.add('comment');
       commentElement.textContent = commentText;
-  
+      allComments.style.display = "block";
       allComments.insertBefore(commentElement, allComments.firstChild);
   
       commentInput.value = '';
